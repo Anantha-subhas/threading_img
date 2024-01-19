90bd407be6a30574b2c8e5b6ce665f62675c72da
Anantha subhas <139669868+Anantha-subhas@users.noreply.github.com>
2024-01-19 07:34:33 +0530

    file_renamed

---
+++ pillow_image/main.py
@@ -0,0 +1,13 @@
+# import sys
+
+# Access command-line arguments
+# script_name = sys.argv[0]
+# arguments = sys.argv[1:]
+
+# print(f"Script Name: {script_name}")
+# print(f"Arguments: {arguments}")
+
+import os
+
+print(os.getcwd())
+
