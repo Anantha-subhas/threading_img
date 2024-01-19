518cbc679637682c16a1ef06d527b9265d90f107
Anantha subhas <139669868+Anantha-subhas@users.noreply.github.com>
2024-01-19 08:11:10 +0530

    just checking

--- pillow_image/main.py
+++ pillow_image/main.py
@@ -1,13 +1,1 @@
-# import sys
-
-# Access command-line arguments
-# script_name = sys.argv[0]
-# arguments = sys.argv[1:]
-
-# print(f"Script Name: {script_name}")
-# print(f"Arguments: {arguments}")
-
-import os
-
-print(os.getcwd())
-
+print("hello")
