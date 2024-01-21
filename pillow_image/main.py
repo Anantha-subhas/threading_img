ed8802d41210549c0844185cf3a40464805e2590
Anantha subhas <139669868+Anantha-subhas@users.noreply.github.com>
2024-01-21 07:45:06 +0530

    "processing image"

--- pillow_image/main.py
+++ pillow_image/main.py
@@ -1,1 +1,19 @@
-print("hello")
+from PIL import Image
+import os
+
+# image1 = Image.open("images/img1.jpeg")
+# image1.show()
+
+# image1.save("images/img1.png")
+
+# print(os.listdir("images")) this shows all the file inside an img directory (".") => <entire dir>
+
+# Create a new directory for saving PNGs
+output_directory = "pngs"
+os.makedirs(output_directory, exist_ok=True)
+
+for file in os.listdir("images"):
+    if file.endswith(".jpeg") or file.endswith(".jpg"):  # both jpeg and jpg
+        i = Image.open(f"images/{file}")  # open an img file
+        i_name, i_exe = os.path.splitext(file)  # separated by a dot eg: img1.jpeg "img1" "jpeg"
+        i.save(f"{output_directory}/{i_name}.png")  # already created pngs_dir
